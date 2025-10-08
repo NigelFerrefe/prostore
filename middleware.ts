9d78f2c5ad25d3bd356885810e8811b5bb05b9d9
NigelFerrefe <nferrefe@gmail.com>
2025-10-08 19:06:52 +0200

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,23 @@
-export { auth as middleware} from '@/auth';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+/*   if (req.nextUrl.pathname.startsWith("/admin") && token.role !== "admin") {
+    return NextResponse.redirect(new URL("/", req.url));
+  } */
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up).*)",
+  ],
+};
